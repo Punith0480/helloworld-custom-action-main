70059cf0fb236126f0818f7be282e92b8e4bf050
Punith0480 <95130310+Punith0480@users.noreply.github.com>
2021-11-29 23:49:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -28,11 +28,11 @@ try {
   
    var gitHubRepository = core.getInput('GitHubRepository');
    console.log(`gitHubRepository ${gitHubRepository}`);
-  var gitRepoOutPut = core.setOutput("gitHubRepository", gitHubRepository);
+ // var gitRepoOutPut = core.setOutput("gitHubRepository", gitHubRepository);
   
   async function GetCommitCountByPR() {
   let url="https://api.github.com/repos/";
-  let repoName= gitRepoOutPut;
+  let repoName= `${gitHubRepository}`;
   let result=url.concat("", repoName);
   console.log(result);
   const response = await octokitRequest.request('GET https://api.github.com/repos/Punith0480/helloworld-action-main/pulls/3/commits');
