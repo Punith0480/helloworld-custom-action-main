ef85f51e503cf160593095eaec448a50089b0416
Punith0480 <95130310+Punith0480@users.noreply.github.com>
2021-11-29 23:36:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// console.log('Hello, world!');
 const core = require('@actions/.core-wdJd1Avc');
 const github = require('@actions/.github-O1rVNLeZ');
 const fetch = require('node-fetch');
@@ -6,8 +5,6 @@ const octokit = require('@octokit/core');
 var async = require('asyncawait/async');
 var await = require('asyncawait/await');
 const octokitRequest = require('@octokit/request');
-//import fetch from 'node-fetch';
-//var token = "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"
 
 try {
   // `who-to-greet` input defined in action metadata file
@@ -37,7 +34,7 @@ try {
   let url="https://api.github.com/repos/";
   let repoName=`${gitRepoOutPut}`;
   let result=url.concat("", repoName);
-  console.log(${result});
+  console.log(result);
   const response = await octokitRequest.request('GET https://api.github.com/repos/Punith0480/helloworld-action-main/pulls/3/commits');
  // console.log(`response ${response}`);
   data1 = JSON.stringify(response);
@@ -102,45 +99,6 @@ async function applyLabel() {
   console.log('End of fuction');
 })();
   
- //async().then(data => console.log(data))
-
-  /*
- const url = "https://api.github.com/repos/PunithMohan/helloworld-action/pulls/1/commits";
- console.log(`url ${url}`);
- const options = {
-  headers: {
-    Authorization: "token ${Github_Token}"
-  }
-};
-
-
-fetch(url, options)
-  .then( res => res.json() )
-  .then( data => console.log(data) );
-  */
-  /*
-  async function postData(url = '' , data = {}) {
-  // Default options are marked with *
-  const response = await fetch(url, {
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    mode: 'cors', // no-cors, *cors, same-origin
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: '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', // include, *same-origin, omit
-    headers: {
-      'Content-Type': 'application/json'
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    redirect: 'follow', // manual, *follow, error
-    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    body: JSON.stringify(data) // body data type must match "Content-Type" header
-  });
-  return response.json(); // parses JSON response into native JavaScript objects
-}
-  postData('https://api.github.com/repos/PunithMohan/helloworld-action/pulls/1/commits', {})
-  .then(data => {
-    console.log(data); // JSON data parsed by `data.json()` call
-  });
-  */
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
  // console.log(`The event payload: ${payload}`);
