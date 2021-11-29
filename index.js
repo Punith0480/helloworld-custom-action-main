071217006bc08a1c1e4a59984685ef426ab69c9e
Punith0480 <95130310+Punith0480@users.noreply.github.com>
2021-11-29 08:00:09 +0530

    removed

--- index.js
+++ index.js
@@ -44,10 +44,7 @@ try {
 (async() => {
   console.log('before start');
   var Data = myfunction().then(data => console.log(data));
-  //console.log(JSON.stringify(Data, undefined, 2));
-  var data1 = JSON.parse(Data);
-  console.log(data1);
-  
+  console.log(JSON.stringify(Data, undefined, 2));
   console.log('after start');
 })();
   
